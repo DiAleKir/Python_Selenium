a653a3dc1607c64f07bb5b7171228900a96485d0
DiAleKir <Nameless12628@gmail.com>
2025-06-14 17:08:05 +0300

    -Fix conftest.py(skipping a function call) -Add pages folder -Add base_page.py -Created BasePage class

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome
+    driver = webdriver.Chrome()
     driver.maximize_window()
     yield driver
     driver.quit()
