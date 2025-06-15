71198b997befb3d0360ea54a1035aa8794d9eccd
DiAleKir <Nameless12628@gmail.com>
2025-06-15 16:29:19 +0300

    -Added assert to func "test_text_box"

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -7,4 +7,6 @@ class TestElements:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
-            text_box_page.fill_fields()
+            input_data = text_box_page.fill_fields()
+            output_data = text_box_page.check_filled_form()
+            assert input_data == output_data
