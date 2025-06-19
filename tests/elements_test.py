a948cb351fee6b3859cc500971554168f7402d8f
DiAleKir <Nameless12628@gmail.com>
2025-06-19 14:38:48 +0300

    -Update elements_test.py(Added class "TestCheckBox" -Added class "CheckBoxPageLocators" -Added class "CheckBoxPage" -Fixed def "scroll_to_element" in BasePage

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,4 +1,5 @@
-from pages.element_page import TextBoxPage
+import re
+from pages.element_page import TextBoxPage, CheckBoxPage
 
 
 class TestElements:
@@ -9,4 +10,14 @@ class TestElements:
             text_box_page.open()
             input_data = text_box_page.fill_fields()
             output_data = text_box_page.check_filled_form()
-            assert input_data == output_data
+            assert input_data == output_data, "The data doesn't match"
+
+    class TestChechBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            assert input_checkbox == output_result, "Checkboxes haven't been selected "
