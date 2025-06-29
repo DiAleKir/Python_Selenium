6863ce07d735c9db6fc93e105fb4b80e26f6b2b3
DiAleKir <Nameless12628@gmail.com>
2025-06-29 17:40:39 +0300

    Update widgets_test.py -Added class TestSortable
    Update widgets_page.py
    -Added class SortablePage
    
    Update widgets_locators.py
    -Added class SortablePageLocators
    
    Update base_page.py
    -Added def drag_and_drop

---
+++ locators/interactions_locators.py
@@ -0,0 +1,6 @@
+class SortablePageLocators:
+
+    TAB_LIST = ('xpath', '//a[@id="demo-tab-list"]')
+    ITEM_LIST = ('xpath', '//div[@class="vertical-list-container mt-4"]/div')
+    TAB_GRID = ('xpath', '//a[@id="demo-tab-grid"]')
+    ITEM_GRID = ('xpath', '//div[@class="create-grid"]/div')
