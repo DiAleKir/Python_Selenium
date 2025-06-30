4550c6addfd22a42e03330dbf8688b9e3d2b9eba
DiAleKir <Nameless12628@gmail.com>
2025-06-30 14:43:10 +0300

    Update widgets_test.py -Added class TestSelectable
    Update widgets_page.py
    -Added class SelectablePage
    
    Update widgets_locators.py
    -Added class SelectablePageLocators
    
    Update base_page.py
    -Added def drag_and_drop

--- tests/interactions_test.py
+++ tests/interactions_test.py
@@ -1,4 +1,4 @@
-from pages.interactions_page import SortablePage
+from pages.interactions_page import SortablePage, SelectablePage
 
 
 class TestInteractions:
@@ -9,10 +9,25 @@ class TestInteractions:
             sortable_page = SortablePage(driver, 'https://demoqa.com/sortable')
             sortable_page.open()
             order_before, order_after = sortable_page.change_list_order()
-            assert order_before != order_after
+            assert order_before != order_after, 'The order has not been changed'
 
         def test_frid_sortable(self, driver):
             sortable_page = SortablePage(driver, 'https://demoqa.com/sortable')
             sortable_page.open()
             order_before, order_after = sortable_page.change_grid_order()
-            assert order_before != order_after
+            assert order_before != order_after, 'The order has not been changed'
+
+
+    class TestSelectable:
+
+        def test_list_selectable(self, driver):
+            selectable_page = SelectablePage(driver, 'https://demoqa.com/selectable')
+            selectable_page.open()
+            active_elements = selectable_page.select_item('list')
+            assert len(active_elements) > 0, 'The elements were not selected'
+
+        def test_grid_selectable(self, driver):
+            selectable_page = SelectablePage(driver, 'https://demoqa.com/selectable')
+            selectable_page.open()
+            active_elements = selectable_page.select_item('grid')
+            assert len(active_elements) > 0, 'The elements were not selected'
