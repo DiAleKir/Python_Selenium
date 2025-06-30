cbdd3de58214283ed4106b2a221201a8c1da64b9
DiAleKir <Nameless12628@gmail.com>
2025-06-30 14:43:10 +0300

    Update interactions_test.py -Added class TestSelectable
    Update interactions_page.py
    -Added class SelectablePage
    
    Update interactions_locators.py
    -Added class SelectablePageLocators

--- locators/interactions_locators.py
+++ locators/interactions_locators.py
@@ -4,3 +4,13 @@ class SortablePageLocators:
     ITEM_LIST = ('xpath', '//div[@class="vertical-list-container mt-4"]/div')
     TAB_GRID = ('xpath', '//a[@id="demo-tab-grid"]')
     ITEM_GRID = ('xpath', '//div[@class="create-grid"]/div')
+
+
+class SelectablePageLocators:
+
+    TAB_LIST = ('xpath', '//a[@id="demo-tab-list"]')
+    ITEM_LIST = ('xpath', '//div[@id="demo-tabpane-list"]//li')
+    ITEM_LIST_ACTIVE = ('xpath', '//div[@id="demo-tabpane-list"]//li[contains(@class, "active")]')
+    TAB_GRID = ('xpath', '//a[@id="demo-tab-grid"]')
+    ITEM_GRID = ('xpath', '//div[@id="gridContainer"]//li')
+    ITEM_GRID_ACTIVE = ('xpath', '//div[@id="gridContainer"]//li[contains(@class, "active")]')
--- pages/interactions_page.py
+++ pages/interactions_page.py
@@ -1,6 +1,6 @@
 import random
 
-from locators.interactions_locators import SortablePageLocators
+from locators.interactions_locators import SortablePageLocators, SelectablePageLocators
 from pages.base_page import BasePage
 
 
@@ -17,12 +17,36 @@ class SortablePage(BasePage):
         item_list = random.sample(self.elements_are_visible(self.locators.ITEM_LIST), k=2)
         self.drag_and_drop(item_list[0], item_list[1])
         order_after = self.get_sortable_items(self.locators.ITEM_LIST)
-        return order_before, order_after, 'The order has not been changed'
+        return order_before, order_after
 
     def change_grid_order(self):
         self.element_is_visible(self.locators.TAB_GRID).click()
         order_before = self.get_sortable_items(self.locators.ITEM_GRID)
-        item_list = random.sample(self.elements_are_visible(self.locators.ITEM_GRID), k=2)
-        self.drag_and_drop(item_list[0], item_list[1])
+        item_grid = random.sample(self.elements_are_visible(self.locators.ITEM_GRID), k=2)
+        self.drag_and_drop(item_grid[0], item_grid[1])
         order_after = self.get_sortable_items(self.locators.ITEM_GRID)
-        return order_before, order_after, 'The order has not been changed'
+        return order_before, order_after
+
+
+class SelectablePage(BasePage):
+    locators = SelectablePageLocators()
+
+    def click_selectable_item(self, elements):
+        item_list = self.elements_are_visible(elements)
+        items = random.sample(item_list, k=2)
+        for _ in items:
+            _.click()
+
+    def select_item(self, tab):
+        if tab == 'list':
+            self.element_is_visible(self.locators.TAB_LIST).click()
+            self.click_selectable_item(self.locators.ITEM_LIST)
+            active_elements = self.elements_are_visible(self.locators.ITEM_LIST_ACTIVE)
+        elif tab == 'grid':
+            self.element_is_visible(self.locators.TAB_GRID).click()
+            self.click_selectable_item(self.locators.ITEM_GRID)
+            active_elements = self.elements_are_visible(self.locators.ITEM_GRID_ACTIVE)
+        active_list = []
+        for element in active_elements:
+            active_list.append(element.text)
+        return active_list
